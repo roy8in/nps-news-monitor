5be6136a22755745ba5b87b1b4e128151c97d883
Roy <72531163+roy8in@users.noreply.github.com>
2025-12-21 18:43:40 +0900

    Update nps_news_bot.py

--- nps_news_bot.py
+++ nps_news_bot.py
@@ -17,7 +17,7 @@ GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
 
 # Gemini AI 설정
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 KEYWORD = '"국민연금" "김성주"'
 FILE_PATH = "last_link.txt"
